6c9c71fed88ebc9fa01ea6ad7d9d115ec502f8ec
fdiallo <fallaye.diallo@gmail.com>
2026-05-10 16:30:38 -0400

    Added TodoContextType interface

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -5,4 +5,17 @@ export interface Todo {
   completed: boolean;
 }
 
+export interface TodoContextType {
+  todos: Todo[];
+  filter: FilterType;
+  darkMode: boolean;
+  addTodo: (text: string) => void;
+  deleteTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
+  editTodo: (id: string, newText: string) => void;
+  setFilter: (filter: FilterType) => void;
+  toggleTheme: () => void;
+  remainingCount: number;
+}
+
 export type FilterType = 'all' | 'active' | 'completed';
